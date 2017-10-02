1f112ab5dc4e06441eafb5ad58d43b80fabb2d62
dima <dima@ip-home.net>
2017-10-02 17:33:28 +0300

    js v2

--- resources/views/parts/js.blade.php
+++ resources/views/parts/js.blade.php
@@ -19,7 +19,7 @@
 <script src="/assets/global/plugins/jquery.pulsate.min.js" type="text/javascript"></script>
 <script src="/assets/layouts/layout5/scripts/layout.min.js" type="text/javascript"></script>
 <script src="/assets/global/plugins/jquery-cookie-1.4.1/jquery.cookie.js"></script>
-<script src="/assets/global/scripts/common.js" type="text/javascript"></script>
+<script src="/assets/global/scripts/common.js?v2" type="text/javascript"></script>
 <script src="//juk.edska.ru:8443/socket.io/socket.io.js" type="text/javascript"></script>
 <script src="/assets/global/scripts/asterisk.js" type="text/javascript"></script>
 <!-- END THEME LAYOUT SCRIPTS -->
