cb3cdd0216d7f3952eb8a37e2c8bcd5b27da2b66
dima <dima@ip-home.net>
2017-10-13 12:14:50 +0300

    рефакторинг

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Traits\RegistersUsers;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
