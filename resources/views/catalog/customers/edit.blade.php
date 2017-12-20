f2f3b6c9be8dfdd11de0a2227cd7f09230e4e5c9
dima <dima@ip-home.net>
2017-12-20 15:52:04 +0300

    +

--- resources/views/catalog/customers/edit.blade.php
+++ resources/views/catalog/customers/edit.blade.php
@@ -82,158 +82,166 @@
 
         </div>
 
-        <ul class="nav nav-tabs">
-            <li class="active">
-                <a data-toggle="tab" href="#tickets">
-                    Обращения
-                    <span class="badge" id="tickets-count">{{ $customer->tickets->count() }}</span>
-                </a>
-            </li>
-            <li>
-                <a data-toggle="tab" href="#calls">
-                    Звонки
-                    <span class="badge" id="calls-count">{{ $customer->calls()->count() }}</span>
-                </a>
-            </li>
-        </ul>
+        @if ( \Auth::user()->canOne( [ 'catalog.customers.tickets', 'catalog.customers.calls' ] ) )
 
-        <div class="tab-content">
-            <div id="tickets" class="tab-pane fade in active">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        @if ( ! $customer->tickets->count() )
-                            @include( 'parts.error', [ 'error' => 'Ничего не найдено' ] )
-                        @else
-                            <table class="table table-hover table-striped">
-                                <thead>
-                                <tr>
-                                    <th>
-                                        Номер заявки
-                                    </th>
-                                    <th>
-                                        Дата заявки
-                                    </th>
-                                    <th>
-                                        Адрес проблемы
-                                    </th>
-                                    <th>
-                                        Тип заявки
-                                    </th>
-                                    <th>
-                                        Статус заявки
-                                    </th>
-                                    <th class="hidden-print">
-                                        &nbsp;
-                                    </th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach( $customer->tickets as $ticket )
+            <ul class="nav nav-tabs">
+                @if ( \Auth::user()->can( 'catalog.customers.tickets' )) )
+                    <li class="active">
+                        <a data-toggle="tab" href="#tickets">
+                            Обращения
+                            <span class="badge" id="tickets-count">{{ $customer->tickets->count() }}</span>
+                        </a>
+                    </li>
+                @endif
+                @if ( \Auth::user()->can( 'catalog.customers.calls' )) )
+                    <li>
+                        <a data-toggle="tab" href="#calls">
+                            Звонки
+                            <span class="badge" id="calls-count">{{ $customer->calls()->count() }}</span>
+                        </a>
+                    </li>
+                @endif
+            </ul>
+
+            <div class="tab-content">
+                <div id="tickets" class="tab-pane fade in active">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            @if ( ! $customer->tickets->count() )
+                                @include( 'parts.error', [ 'error' => 'Ничего не найдено' ] )
+                            @else
+                                <table class="table table-hover table-striped">
+                                    <thead>
                                     <tr>
-                                        <td>
-                                            <a href="{{ route( 'tickets.show', $ticket->id ) }}" target="_blank">
-                                                {{ $ticket->id }}
-                                            </a>
-                                        </td>
-                                        <td>
-                                            {{ $ticket->created_at->format( 'd.m.Y H:i' ) }}
-                                        </td>
-                                        <td>
-                                            {{ $ticket->getAddress() }}
-                                            <span class="small text-muted">
-                                        ({{ $ticket->getPlace() }})
-                                    </span>
-                                        </td>
-                                        <td>
-                                            <div class="bold">{{ $ticket->type->category->name }}</div>
-                                            {{ $ticket->type->name }}
-                                        </td>
-                                        <td>
-                                    <span class="text-{{ $ticket->getClass() }}">
-                                        {{ $ticket->status_name }}
-                                    </span>
-                                        </td>
-                                        <td class="text-right hidden-print">
-                                            <a href="{{ route( 'tickets.show', $ticket->id ) }}" class="btn btn-lg btn-primary tooltips" title="Открыть обращение #{{ $ticket->id }}" target="_blank">
-                                                <i class="fa fa-chevron-right"></i>
-                                            </a>
-                                        </td>
+                                        <th>
+                                            Номер заявки
+                                        </th>
+                                        <th>
+                                            Дата заявки
+                                        </th>
+                                        <th>
+                                            Адрес проблемы
+                                        </th>
+                                        <th>
+                                            Тип заявки
+                                        </th>
+                                        <th>
+                                            Статус заявки
+                                        </th>
+                                        <th class="hidden-print">
+                                            &nbsp;
+                                        </th>
                                     </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        @endif
+                                    </thead>
+                                    <tbody>
+                                    @foreach( $customer->tickets as $ticket )
+                                        <tr>
+                                            <td>
+                                                <a href="{{ route( 'tickets.show', $ticket->id ) }}" target="_blank">
+                                                    {{ $ticket->id }}
+                                                </a>
+                                            </td>
+                                            <td>
+                                                {{ $ticket->created_at->format( 'd.m.Y H:i' ) }}
+                                            </td>
+                                            <td>
+                                                {{ $ticket->getAddress() }}
+                                                <span class="small text-muted">
+                                            ({{ $ticket->getPlace() }})
+                                        </span>
+                                            </td>
+                                            <td>
+                                                <div class="bold">{{ $ticket->type->category->name }}</div>
+                                                {{ $ticket->type->name }}
+                                            </td>
+                                            <td>
+                                        <span class="text-{{ $ticket->getClass() }}">
+                                            {{ $ticket->status_name }}
+                                        </span>
+                                            </td>
+                                            <td class="text-right hidden-print">
+                                                <a href="{{ route( 'tickets.show', $ticket->id ) }}" class="btn btn-lg btn-primary tooltips" title="Открыть обращение #{{ $ticket->id }}" target="_blank">
+                                                    <i class="fa fa-chevron-right"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            @endif
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div id="calls" class="tab-pane fade">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        @if ( ! $customer->calls()->count() )
-                            @include( 'parts.error', [ 'error' => 'Ничего не найдено' ] )
-                        @else
-                            <table class="table table-hover table-striped">
-                                <thead>
-                                    <tr>
-                                        <th rowspan="2">
-                                            Дата звонка
-                                        </th>
-                                        <th colspan="2" class="text-center">
-                                            Оператор
-                                        </th>
-                                        @if ( \Auth::user()->can( 'calls' ) )
+                <div id="calls" class="tab-pane fade">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            @if ( ! $customer->calls()->count() )
+                                @include( 'parts.error', [ 'error' => 'Ничего не найдено' ] )
+                            @else
+                                <table class="table table-hover table-striped">
+                                    <thead>
+                                        <tr>
                                             <th rowspan="2">
-                                                Запись
+                                                Дата звонка
                                             </th>
-                                        @endif
-                                    </tr>
-                                    <tr>
-                                        <th>
-                                            ФИО
-                                        </th>
-                                        <th>
-                                            Номер
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                @foreach ( $customer->calls() as $call )
-                                    <tr>
-                                        <td>
-                                            {{ \Carbon\Carbon::parse( $call->calldate )->format( 'd.m.Y H:i' ) }}
-                                        </td>
-                                        <td>
-                                            @if ( $call->queueLog->operator() )
-                                                {{ $call->queueLog->operator()->getShortName() }}
-                                            @else
-                                                -
+                                            <th colspan="2" class="text-center">
+                                                Оператор
+                                            </th>
+                                            @if ( \Auth::user()->can( 'calls' ) )
+                                                <th rowspan="2">
+                                                    Запись
+                                                </th>
                                             @endif
-                                        </td>
-                                        <td>
-                                            {{ $call->queueLog->number() }}
-                                        </td>
-                                        @if ( \Auth::user()->can( 'calls' ) )
+                                        </tr>
+                                        <tr>
+                                            <th>
+                                                ФИО
+                                            </th>
+                                            <th>
+                                                Номер
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach ( $customer->calls() as $call )
+                                        <tr>
                                             <td>
-                                                @if ( $call->hasMp3() )
-                                                    <a href="{{ $call->getMp3() }}" target="_blank">
-                                                        {{ $call->getMp3() }}
-                                                    </a>
+                                                {{ \Carbon\Carbon::parse( $call->calldate )->format( 'd.m.Y H:i' ) }}
+                                            </td>
+                                            <td>
+                                                @if ( $call->queueLog->operator() )
+                                                    {{ $call->queueLog->operator()->getShortName() }}
                                                 @else
-                                                    <span class="text-danger">
-                                                Запись не найдена
-                                            </span>
+                                                    -
                                                 @endif
                                             </td>
-                                        @endif
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        @endif
+                                            <td>
+                                                {{ $call->queueLog->number() }}
+                                            </td>
+                                            @if ( \Auth::user()->can( 'calls' ) )
+                                                <td>
+                                                    @if ( $call->hasMp3() )
+                                                        <a href="{{ $call->getMp3() }}" target="_blank">
+                                                            {{ $call->getMp3() }}
+                                                        </a>
+                                                    @else
+                                                        <span class="text-danger">
+                                                    Запись не найдена
+                                                </span>
+                                                    @endif
+                                                </td>
+                                            @endif
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            @endif
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+        @endif
 
     @else
 
