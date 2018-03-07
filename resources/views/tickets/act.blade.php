7f982ea12d8d52dcd8b9e50c4565058bb8470fba
dima <dima@ip-home.net>
2018-03-07 12:41:45 +0300

    +

--- resources/views/tickets/act.blade.php
+++ resources/views/tickets/act.blade.php
@@ -10,20 +10,16 @@
     <title>{{ \App\Classes\Title::render() }}</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="{{ \Config::get( 'app.name' ) }} - Акт" name="description" />
-    <meta content="" name="author" />
+    <meta content="{{ \Config::get( 'app.author' ) }}" name="author" />
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
     <link href="/assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
     <!-- END GLOBAL MANDATORY STYLES -->
     <!-- BEGIN THEME GLOBAL STYLES -->
     <link href="/assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
-    <link href="/assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
     <!-- END THEME GLOBAL STYLES -->
     <!-- BEGIN PAGE LEVEL STYLES -->
-    <link href="/assets/pages/css/act.css?v2" rel="stylesheet" type="text/css" />
+    <link href="/assets/pages/css/act.css?v3" rel="stylesheet" type="text/css" />
     <!-- END PAGE LEVEL STYLES -->
 </head>
 <!-- END HEAD -->
