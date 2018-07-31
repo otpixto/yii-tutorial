655596f3b3fc67b126d0289abe7b63ef6333fdb1
dima <dima@ip-home.net>
2018-07-31 14:43:48 +0300

    +

--- resources/views/works/parts/search.blade.php
+++ resources/views/works/parts/search.blade.php
@@ -1,4 +1,4 @@
-{!! Form::open( [ 'method' => 'post', 'class' => 'submit-loading', 'url' => route( 'works.filter' ), 'id' => 'search-form' ] ) !!}
+{!! Form::open( [ 'method' => 'post', 'url' => route( 'works.filter' ), 'id' => 'search-form' ] ) !!}
 {!! Form::hidden( 'search', 1 ) !!}
 <div class="row margin-top-10">
     <h4 class="col-md-2">
