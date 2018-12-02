b10b1541bedc1b6706963a4d9cd2c384ffed24cc
dima <dima@ip-home.net>
2018-12-02 13:27:34 +0300

    version

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- BEGIN FOOTER -->
 <p class="copyright hidden-print">
-    {{ date( 'Y' ) }} &copy; {{ \Config::get( 'app.name' ) }}
+    {{ date( 'Y' ) }} &copy; {{ config( 'app.name' ) }} {{ config( 'app.version' ) }}
 </p>
 <a href="#index" class="go2top hidden-print">
     <i class="icon-arrow-up"></i>
