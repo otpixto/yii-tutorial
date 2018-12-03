491e08cd6026f9119068c21e675beafe0bb78b7b
dima <dima@ip-home.net>
2018-12-03 15:27:48 +0300

    bot try catch

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -20,72 +20,81 @@ class BotController extends Controller
 
         if ( $token != \Config::get( 'telegram.bot_token' ) ) return;
 
-        $update = $this->telegram->commandsHandler( true );
-        $message = $update->getMessage();
-        if ( ! $message ) return;
-        $chat = $message->getChat();
-        if ( ! $chat ) return;
-
-        $telegram_id = $chat->getId();
-
-        $attributes = [
-            'telegram_id' => $telegram_id,
-            'first_name' => $chat->getFirstName() ?? null,
-            'last_name' => $chat->getLastName() ?? null,
-            'username' => $chat->getUsername()
-        ];
-
-        $reply = $message->getReplyToMessage();
-
-        if ( $reply )
+        try
         {
-            $message_id = $reply->getMessageId();
-            $telegram_code = $message->getText();
-            if ( \Cache::has( 'telegram-subscribe-' . $message_id ) && \Cache::get( 'telegram-subscribe-' . $message_id ) == $telegram_id )
+
+            $update = $this->telegram->commandsHandler( true );
+            $message = $update->getMessage();
+            if ( ! $message ) return;
+            $chat = $message->getChat();
+            if ( ! $chat ) return;
+
+            $telegram_id = $chat->getId();
+
+            $attributes = [
+                'telegram_id' => $telegram_id,
+                'first_name' => $chat->getFirstName() ?? null,
+                'last_name' => $chat->getLastName() ?? null,
+                'username' => $chat->getUsername()
+            ];
+
+            $reply = $message->getReplyToMessage();
+
+            if ( $reply )
             {
-                \Cache::forget( 'telegram-subscribe-' . $message_id );
-                $res = Management::telegramSubscribe( $telegram_code, $attributes );
-                if ( $res instanceof MessageBag )
+                $message_id = $reply->getMessageId();
+                $telegram_code = $message->getText();
+                if ( \Cache::has( 'telegram-subscribe-' . $message_id ) && \Cache::get( 'telegram-subscribe-' . $message_id ) == $telegram_id )
                 {
-                    $text = $res->first();
+                    \Cache::forget( 'telegram-subscribe-' . $message_id );
+                    $res = Management::telegramSubscribe( $telegram_code, $attributes );
+                    if ( $res instanceof MessageBag )
+                    {
+                        $text = $res->first();
+                    }
+                    else
+                    {
+                        $text = 'Подписка успешно оформлена';
+                    }
+                    $this->telegram->sendMessage([
+                        'chat_id' => $telegram_id,
+                        'text' => $text
+                    ]);
                 }
-                else
+                else if ( \Cache::has( 'telegram-unsubscribe-' . $message_id ) && \Cache::get( 'telegram-unsubscribe-' . $message_id ) == $telegram_id )
                 {
-                    $text = 'Подписка успешно оформлена';
+                    \Cache::forget( 'telegram-unsubscribe-' . $message_id );
+                    $res = Management::telegramUnSubscribe( $telegram_code, $attributes );
+                    if ( $res instanceof MessageBag )
+                    {
+                        $text = $res->first();
+                    }
+                    else
+                    {
+                        $text = 'Подписка успешно отменена';
+                    }
+                    $this->telegram->sendMessage([
+                        'chat_id' => $telegram_id,
+                        'text' => $text
+                    ]);
                 }
-                $this->telegram->sendMessage([
-                    'chat_id' => $telegram_id,
-                    'text' => $text
-                ]);
             }
-            else if ( \Cache::has( 'telegram-unsubscribe-' . $message_id ) && \Cache::get( 'telegram-unsubscribe-' . $message_id ) == $telegram_id )
-            {
-                \Cache::forget( 'telegram-unsubscribe-' . $message_id );
-                $res = Management::telegramUnSubscribe( $telegram_code, $attributes );
-                if ( $res instanceof MessageBag )
-                {
-                    $text = $res->first();
-                }
-                else
-                {
-                    $text = 'Подписка успешно отменена';
-                }
-                $this->telegram->sendMessage([
-                    'chat_id' => $telegram_id,
-                    'text' => $text
-                ]);
-            }
+
+            /*$this->telegram->sendChatAction([
+                'chat_id' => $update->getMessage()->getChat()->getId(),
+                'action' => 'typing'
+            ]);
+
+            $this->telegram->sendMessage([
+                'chat_id' => $update->getMessage()->getChat()->getId(),
+                'text' => $update->getMessage()->getText()
+            ]);*/
+
         }
-
-        /*$this->telegram->sendChatAction([
-            'chat_id' => $update->getMessage()->getChat()->getId(),
-            'action' => 'typing'
-        ]);
-
-        $this->telegram->sendMessage([
-            'chat_id' => $update->getMessage()->getChat()->getId(),
-            'text' => $update->getMessage()->getText()
-        ]);*/
+        catch ( \Exception $e )
+        {
+            return $e->getMessage();
+        }
 
     }
 
