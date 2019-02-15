92f73f3b628f225006956381a34d0b2262bbc2ea
dima <dima@ip-home.net>
2019-02-15 17:03:03 +0300

    +

--- app/Classes/Mosreg.php
+++ app/Classes/Mosreg.php
@@ -10,16 +10,16 @@ class Mosreg
     private $curl = false;
 
     private $statuses = [
-        'IN_WORK'                       => 'В работе',
-        'ANSWERED'                      => 'Ожидает подтверждения',
-        'EXPIRED'                       => 'Срок превышен',
-        'GZI_REMEDY'                    => 'Контроль ГЖИ: Устранение',
-        'GZI_REMEDY_ANSWER'             => 'Контроль ГЖИ: Получен ответ УК',
-        'GZI_EXPIRED'                   => 'Контроль ГЖИ: Просрочено',
-        'GZI_EXTRA_AUDIT'               => 'Контроль ГЖИ: Внеплановая проверка',
-        'UNSATISFIED'                   => 'Требуется доработка',
-        'SOLVED'                        => 'Закрыто',
-        'UNSATISFIED_SENDED_TO_DD'      => 'Несогласие жителя. Отправлено в Добродел.',
+        'IN_WORK' => 'В работе',
+        'ANSWERED' => 'Ожидает подтверждения',
+        'EXPIRED' => 'Срок превышен',
+        'GZI_REMEDY' => 'Контроль ГЖИ: Устранение',
+        'GZI_REMEDY_ANSWER' => 'Контроль ГЖИ: Получен ответ УК',
+        'GZI_EXPIRED' => 'Контроль ГЖИ: Просрочено',
+        'GZI_EXTRA_AUDIT' => 'Контроль ГЖИ: Внеплановая проверка',
+        'UNSATISFIED' => 'Требуется доработка',
+        'SOLVED' => 'Закрыто',
+        'UNSATISFIED_SENDED_TO_DD' => 'Несогласие жителя. Отправлено в Добродел.',
     ];
 
     public function __construct ( $username, $password )
@@ -29,10 +29,10 @@ class Mosreg
 
         $this->curl = curl_init();
 
-        curl_setopt( $this->curl, CURLOPT_URL,self::URL . '/login' );
+        curl_setopt( $this->curl, CURLOPT_URL, self::URL . '/login' );
         $data = [
-            'login-form-email'          => $username,
-            'login-form-password'       => $password,
+            'login-form-email' => $username,
+            'login-form-password' => $password,
         ];
         curl_setopt( $this->curl, CURLOPT_POST, true );
         curl_setopt( $this->curl, CURLOPT_POSTFIELDS, $data );
@@ -54,7 +54,7 @@ class Mosreg
 
     public function normalizeAddress ( $address )
     {
-        return trim( preg_replace('/\s{2,}/U',' ', preg_replace( '/г\.|ул\.|д\.|\,/iU', '', str_replace( ' к. ', 'к', $address ) ) ) );
+        return trim( preg_replace( '/\s{2,}/U', ' ', preg_replace( '/г\.|ул\.|д\.|\,/iU', '', str_replace( ' к. ', 'к', $address ) ) ) );
     }
 
     public function searchAddress ( $address, $normalize = false )
@@ -63,7 +63,7 @@ class Mosreg
         {
             $address = $this->normalizeAddress( $address );
         }
-        curl_setopt( $this->curl, CURLOPT_URL,self::URL . '/api/company/search?address=' . urlencode( $address ) );
+        curl_setopt( $this->curl, CURLOPT_URL, self::URL . '/api/company/search?address=' . urlencode( $address ) );
         curl_setopt( $this->curl, CURLOPT_POST, false );
         $response = curl_exec( $this->curl );
         $err = curl_error( $this->curl );
@@ -77,7 +77,7 @@ class Mosreg
 
     public function getTickets ( $page = 1 )
     {
-        curl_setopt( $this->curl, CURLOPT_URL,self::URL . '/claims' );
+        curl_setopt( $this->curl, CURLOPT_URL, self::URL . '/claims' );
         curl_setopt( $this->curl, CURLOPT_POST, false );
         $response = curl_exec( $this->curl );
         $err = curl_error( $this->curl );
@@ -85,6 +85,39 @@ class Mosreg
         {
             die( 'ERROR: ' . $err );
         }
+        $re = '/<tr class=\".*\" data-claim=\"(.*)\" .*>\s*<td class=".*"><\/td>\s*<td class=".*">\s*<span>(.*)<\/span>\s*<\/td>\s*<td class=".*">\s*<span>(.*)<\/span>\s*<\/td>\s*<td class=".*">\s*<span>(.*)<\/span>\s*<\/td>\s*<td class=".*">\s*<span>(.*)<\/span>\s*<\/td>\s*<td class=".*">\s*<span>(.*)<\/span>\s*<\/td>\s*<td class=".*">\s*<span>(.*)<\/span>\s*<\/td>\s*<td class=".*">\s*<span>(.*)<\/span>\s*<\/td>\s*<td class=".*">\s*<span>(.*)<\/span>\s*<\/td>\s*<td class=".*">\s*<span>(.*)<\/span>\s*<\/td>\s*<td class=".*">\s*<span.*><\/span>\s*<\/td>\s*<td class=".*"><\/td>\s*<\/tr>/m';
+        $data = [];
+        preg_match_all( $re, $response, $matches, PREG_SET_ORDER );
+        foreach ( $matches as $i => $row )
+        {
+            $id = $row[ 1 ];
+            $data [ $id ] = [
+                'number' => $row[ 2 ],
+                'created_at' => $row[ 3 ],
+                'management' => $row[ 4 ],
+                'customer_name' => $row[ 5 ],
+                'customer_type' => $row[ 6 ],
+                'building_name' => $row[ 7 ],
+                'type_name' => $row[ 8 ],
+                'updated_at' => $row[ 9 ],
+                'status_name' => $row[ 10 ],
+            ];
+            $data[ $id ][ 'status_code' ] = array_search( $data[ $id ][ 'status_name' ], $this->statuses );
+        }
+        return $data;
+    }
+
+    public function getTicket ( $id )
+    {
+        curl_setopt( $this->curl, CURLOPT_URL, self::URL . '/claim/' . $id );
+        curl_setopt( $this->curl, CURLOPT_POST, false );
+        $response = curl_exec( $this->curl );
+        $err = curl_error( $this->curl );
+        if ( $err )
+        {
+            die( 'ERROR: ' . $err );
+        }
+        return $response;
     }
 
     public function createTicket ( array $data = [] )
