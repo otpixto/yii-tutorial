fe4980ee9bbf368165c8428af4c1674594f50ab6
dima <dima@ip-home.net>
2019-03-05 19:14:39 +0300

    +

--- app/Http/Controllers/Rest/BaseController.php
+++ app/Http/Controllers/Rest/BaseController.php
@@ -198,7 +198,7 @@ class BaseController extends Controller
                 {
                     return $q
                         ->whereNull( 'ip' )
-                        ->orWhere( 'ip', 'like', '%' . $request->ip() . \DB::raw( '\n' ) . '%' );
+                        ->orWhere( 'ip', 'like', '%' . $request->ip() . "\n" . '%' );
                 })
                 ->where( function ( $q ) use ( $request )
                 {
@@ -210,7 +210,7 @@ class BaseController extends Controller
                         if ( ! empty( $url[ 'host' ] ) )
                         {
                             $q
-                                ->orWhere( 'referer', 'like', '%' . $url[ 'host' ] . \DB::raw( '\n' ) . '%' );
+                                ->orWhere( 'referer', 'like', '%' . $url[ 'host' ] . "\n" . '%' );
                         }
                     }
                     return $q;
