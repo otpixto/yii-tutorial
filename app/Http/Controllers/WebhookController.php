5582e47c74ae557d2112d1e73846b78a3cce9fb4
dima <dima@ip-home.net>
2019-08-09 16:45:43 +0300

    +

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -142,6 +142,7 @@ class WebhookController extends Controller
                     case 'NEW_CLAIM':
                     case 'UNSATISFIED':
                         $mosreg_status = 'IN_WORK';
+                        $mosreg->toWork( $ticketManagement->mosreg_id );
                         break;
                 }
             }
