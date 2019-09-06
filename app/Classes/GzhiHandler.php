c69ce252b4e6cccfa475ec05e6741610a3377cd7
Alexey Sergeev <otpixto@yandex.ru>
2019-09-06 12:26:37 +0300

    Интеграция с ЕАИС 48 #DEV-557

--- app/Classes/GzhiHandler.php
+++ app/Classes/GzhiHandler.php
@@ -102,6 +102,7 @@ class GzhiHandler
     public function handleGzhiTicket ( Ticket $ticket, GzhiApiProvider $gzhiProvider ) : int
     {
 
+        return 0;
         $username = $gzhiProvider->login;
 
         $password = $gzhiProvider->password;
