72f9a1ab37783f49c75d0cac8bca57d5c89fee61
Alexey Sergeev <otpixto@yandex.ru>
2019-09-19 10:30:29 +0300

    Интеграция с ЕАИС 62 #DEV-557

--- app/Classes/GzhiHandler.php
+++ app/Classes/GzhiHandler.php
@@ -144,20 +144,18 @@ class GzhiHandler
             $gzhiRequest = new GzhiRequest();
         }
 
-        $ticket->customer->load( 'buildings' );
-
         $appealGuid = ( ! empty( $gzhiRequest->appeal_guid ) ) ? $gzhiRequest->appeal_guid : (string) Uuid::generate();
 
         $managementGuid = $ticket->managements[ 0 ]->management->parent->gzhi_guid ?? $ticket->managements[ 0 ]->management->parent->guid ?? $ticket->managements[ 0 ]->management->gzhi_guid ?? $ticket->managements[ 0 ]->management->guid;
 
-        //$managementGuid = '357EF0C4-BB06-11E7-9583-B5CD11EEAB0E';
-
         if ( ! isset( $ticket->managements[ 0 ]->management ) || ! $ticket->type->gzhi_code_type || ! $ticket->type->gzhi_code || $ticket->building->gzhi_address_guid == null || $ticket->vendors()
                 ->where( [ 'vendor_id' => GzhiRequest::GZHI_VENDOR_ID ] )
-                ->count() || $ticket->type_id == null || ! in_array( $ticket->status_code, GzhiRequest::GZHI_STATUSES_LIST ) || $managementGuid == '355D5C52-BB06-11E7-9583-B5CD11EEAB0E' || ! $ticket->status->gzhi_status_code )
+                ->count() || $ticket->type_id == null || ! in_array( $ticket->status_code, GzhiRequest::GZHI_STATUSES_LIST ) || $managementGuid == '355D5C52-BB06-11E7-9583-B5CD11EEAB0E' || ! $ticket->status->gzhi_status_code || ! $ticket->customer )
         {
             return 0;
         }
+
+        $ticket->customer->load( 'buildings' );
 
         $actualAddress = $ticket->customer->getActualAddress() ?? '';
 
