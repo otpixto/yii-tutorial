963dd5dbe2e1ba4b5f67ae5153122fd6935915d7
dima <dima@ip-home.net>
2019-10-03 12:22:58 +0300

    +

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\CheckActive::class,
+            \App\Http\Middleware\CheckAdmin::class,
         ],
 
         'srm' => [
