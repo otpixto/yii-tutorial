e2ec10689f17d310b72e3a010f1df203d90b9452
Alexey Sergeev <otpixto@yandex.ru>
2019-10-09 11:34:57 +0300

    Всплывающие окна входящих звонков 10 #dev-588

--- resources/views/parts/phone.blade.php
+++ resources/views/parts/phone.blade.php
@@ -5,13 +5,15 @@
     <div class="margin-right-10" id="number-of-calls">
     <span class="badge badge-danger bold">0</span>&nbsp;&nbsp;
             <i class="fa fa-phone"></i>
-    </div>
-    <div id="popup-calls">
-        <a href="{{ route( 'profile.phone' ) }}" class="btn btn-xs btn-danger" id="phone-state">
-            Разлогинить телефон
-            <span class="bold" id="call-phone"></span>
-        </a>
-        <div id="inner-popup-calls"></div>
+
+        <div id="popup-calls">
+            <a href="{{ route( 'profile.phone' ) }}" class="btn btn-xs btn-danger" id="phone-state">
+                Разлогинить телефон
+                <span class="bold" id="call-phone"></span>
+            </a>
+            <div id="inner-popup-calls"></div>
+        </div>
+
     </div>
     @else
         <div class="btn-phone btn-group margin-right-10">
