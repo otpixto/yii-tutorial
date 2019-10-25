7e9389739a5b736ad599e707bfb998f4fad0078e
Alexey Sergeev <otpixto@yandex.ru>
2019-10-25 10:19:18 +0300

    гжи5

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -51,13 +51,13 @@ class Kernel extends ConsoleKernel
         $schedule->call(function (){
             (new GzhiHandler())->sendGzhiInfo();
         })
-            ->dailyAt('3:00');
+            ->dailyAt('10:21');
 
         $schedule->call( function ()
         {
             ( new GzhiHandler() )->getGzhiRequestsStatus();
         } )
-            ->dailyAt('4:00');
+            ->dailyAt('10:25');
 
     }
 
