6e1e9969aa843c9571f2955ccdc5c2e4ff4c9865
dima <dima@ip-home.net>
2019-11-25 12:25:27 +0300

    MosregSync

---
+++ app/Console/Commands/MosregSync.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\TicketManagement;
+use Illuminate\Console\Command;
+
+class MosregSync extends Command
+{
+    protected $signature = 'mosreg:sync';
+    protected $description = 'Mosreg sync';
+    const STATUSES = [
+        'closed_with_confirm',
+        'closed_without_confirm',
+        'cancel',
+        'GZI_SOLVED',
+        'rejected',
+        'rejected_operator',
+    ];
+    public function handle ()
+    {
+        try
+        {
+            $ticketManagements = TicketManagement
+                ::whereNotNull( 'mosreg_id' )
+                ->whereNotIn( 'status_code', self::STATUSES )
+                ->where( 'mosreg_status', '!=', 'SOLVED' )
+                ->get();
+            foreach ( $ticketManagements as $ticketManagement )
+            {
+                if ( $ticketManagement->management && $ticketManagement->management->hasMosreg( $mosregClient ) )
+                {
+                    $mosregTicket = $mosregClient->getTicket( $ticketManagement->mosreg_id );
+                    if ( $mosregTicket && $mosregTicket->status_code != $ticketManagement->mosreg_status )
+                    {
+                        $ticketManagement->changeMosregStatus( $mosregTicket->status_code );
+                    }
+                }
+            }
+        }
+        catch ( \Exception $e )
+        {
+            dd( $e );
+        }
+    }
+}
