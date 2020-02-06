6994e20823e9fd74bfe8fd5e60b25ae90e8040fc
Alexey Sergeev <otpixto@yandex.ru>
2020-02-06 10:36:46 +0300

    Добавление изменения сегмента для нескольких адресов 2 #dev-689

--- resources/views/parts/queues.blade.php
+++ resources/views/parts/queues.blade.php
@@ -1,3 +1,12 @@
+@if ( \Auth::user()->admin || \Auth::user()->can( 'admin.calls.show' ) )
+        <a href="{{ route( 'admin.missed_calls' ) }}" class="nav-link white font-white">
+            Перезвонить
+        </a>
+        <span>&nbsp;</span>
+        <span class="badge badge-danger left bold mr-5">{{ \App\Models\Asterisk\MissedCall::whereNull( 'call_id' )->count() }}</span>
+    <span> &nbsp; &nbsp; &nbsp; </span>
+@endif
+
 @if ( \App\Models\Provider::getCurrent() && \App\Models\Provider::$current->queue && \Auth::user()->can( 'queues' ) )
     <!-- BEGIN QUEUES -->
     <span id="queues" class="margin-right-10">
