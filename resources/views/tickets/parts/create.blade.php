d6cdd3fcfab9be866c5f70f9ebf4aabd468c7499
Alexey Sergeev <otpixto@yandex.ru>
2020-03-20 10:24:38 +0300

    Изменение / Добавление полей со сроками по Доброделу 7 #dev-793

--- resources/views/tickets/parts/create.blade.php
+++ resources/views/tickets/parts/create.blade.php
@@ -24,11 +24,14 @@
     <div class="col-lg-7">
 
         <div class="form-group">
+
                     {!! Form::label( 'vendor_id', 'Поступило из', [ 'class' => 'control-label col-xs-3' ] ) !!}
-                <div class="col-xs-2">
+                <div class="col-xs-4">
                     {!! Form::select( 'vendor_id', [ null => ' -- выберите из списка -- ' ] + $vendors, \Input::old( 'vendor_id', $ticket->vendor_id ?? null ), [ 'class' => 'form-control autosave', 'id' => 'vendor_id', 'autocomplete' => 'off' ] ) !!}
+
                 </div>
-                <div class="col-xs-7 vendor @if ( ! $ticket->vendor_id ) hidden @php $required = ''; @endphp @else @php $required = 'required'; @endphp @endif">
+
+                <div class="col-xs-9 col-md-9 col-md-offset-3 margin-top-10 vendor @if ( ! $ticket->vendor_id ) hidden @php $required = ''; @endphp @else @php $required = 'required'; @endphp @endif">
                     <div class="input-group">
                         <span class="input-group-addon al-group-span">№</span>
                         {!! Form::text( 'vendor_number', \Input::old( 'vendor_number', $ticket->vendor_number ), [ 'class' => 'form-control autosave al-group-span', $required,  'oninvalid' => 'this.setCustomValidity("Укажите номер обращения")', 'oninput' => 'this.setCustomValidity("")', 'placeholder' => '№', 'id' => 'vendor_number', 'autocomplete' => 'off' ] ) !!}
@@ -38,6 +41,7 @@
                         {!! Form::date( 'vendor_decision_date', \Input::old( 'vendor_decision_date', $ticket->vendor_decision_date ), [ 'class' => 'form-control autosave al-group-span', $required, 'oninvalid' => 'this.setCustomValidity("Укажите срок решения")', 'oninput' => 'this.setCustomValidity("")', 'placeholder' => 'Срок решения', 'id' => 'vendor_decision_date', 'autocomplete' => 'off' ] ) !!}
                     </div>
                 </div>
+
         </div>
 
         <div class="form-group">
